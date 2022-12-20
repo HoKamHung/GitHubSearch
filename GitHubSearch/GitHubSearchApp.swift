63876134289a644260aac871c1472de6ef542d95
Kevin Ho <kevinho0236@gmail.com>
2022-12-20 19:07:39 +0900

    Initial Commit

---
+++ GitHubSearch/GitHubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSearchApp.swift
+//  GitHubSearch
+//
+//  Created by Kam Hung Ho on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
